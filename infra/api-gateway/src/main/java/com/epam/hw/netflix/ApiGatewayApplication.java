3acae22b9c7ef73d1b8f7e2d63dec1a514116479
Konstantin Novozhilov <Konstantin_Novozhilov@epam.com>
2020-03-02 13:26:31 +0400

    Configured Gateway with Zuul proxy

--- infra/api-gateway/src/main/java/com/epam/hw/netflix/ApiGatewayApplication.java
+++ infra/api-gateway/src/main/java/com/epam/hw/netflix/ApiGatewayApplication.java
@@ -2,8 +2,11 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
